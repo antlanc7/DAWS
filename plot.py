5ffaf76841126521ab02466ac3b68aec0c1baedb
antlanc7 <antonio.lanciotti@live.it>
2021-11-10 18:23:19 +0100

    oggi

---
+++ plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+with open(sys.argv[1],"r") as f:
+    s = f.read().split(";")
+
+samples = []
+for i in range(len(samples)-1):
+    try:
+        samples[i] = int(samples[i])
+    except: pass
+
+plt.plot(n)
+plt.show()
--- server.py
+++ server.py
@@ -9,7 +9,9 @@ import threading
 #classe che raccoglie i dati ricevuti dai diversi sensori attraverso i socket
 class Database: 
     def __init__(self):
+        self.conns = []
         self.data = []
+        self.files = []
         self.l = threading.Lock()
 
     def band_pass_filter(s) -> np.ndarray:
@@ -26,40 +28,67 @@ class Database:
 
         return y
     
-    def new_client(self) -> int:
+    def new_client(self,conn) -> int:
         with self.l:    # prende il lock sulla lista
             n = len(self.data)
+            self.conns.append(conn)
             self.data.append([])
+            self.files.append(open(f"{n}.csv","w"))
         return n
 
     def update(self,id,data):
         self.data[id].extend(data)
+        if len(self.data[id])>2048:
+            self.save_to_file(id)
+            self.data[id].clear()
+            
+    def save_to_file(self,id):
+        self.files[id].write(";".join(map(str,self.data[id]))+";")
+        
+    def close_client(self, id):
+        self.files[id].close()
+        self.conns[id].close()
+        
+    def close_all(self):
+        for id in range(len(self.files)):
+            self.files[id].close()
+            self.conns[id].close()
+            del(self.data[id])
 
 def acceptation_thread(socket, database):
     while True:
         conn, addr = s.accept()
         threading.Thread(target=threaded_client, args=(conn, addr, database)).start()
-
+        
 
 def threaded_client(conn:socket.socket, addr, database: Database):
-    id = database.new_client()
+    id = database.new_client(conn)
     print(f"Il client {addr} si è connesso con ID={id}")
 
     while True:
-        data = conn.recv(4096)
+        data = conn.recv(128)
         n = len(data)
         if n == 0:
             break
         print(f"ID={id} - Ricevuti {n} bytes")
+        print(data)
         try:
-            data_list = pickle.loads(data)
-        except pickle.UnpicklingError:
+            data_list = list(data)
+            data_parsed = []
+            for i in range(0, n, 2):
+                data_parsed.append((data_list[i+1] << 8) + data_list[i])
+        except Exception as e:
+            print(e)
             print("Dati corrotti")
             continue
-        print(f"ID={id} - Lunghezza vettore: {len(data_list)}")
-        database.update(id, data_list)
+        print(f"ID={id} - Lunghezza vettore: {len(data_parsed)}")
+        print(data_parsed)
+        database.update(id, data_parsed)
 
     print(f"Il client {addr} si è disconnesso")
+    print("Salvataggio file in corso...")
+    database.close_client(id)
+    print("Salvataggio completato")
     conn.close()
 
 if __name__=="__main__":
@@ -69,11 +98,11 @@ if __name__=="__main__":
     try:
         s.bind(("", PORT))
     except socket.error as e:
+        print('ziopony')
         print(e)
 
     s.listen()
-    print(
-        f"Server avviato su: {socket.gethostbyname(socket.gethostname())}:{PORT}")
+    print(f"Server avviato sulla porta {PORT}")
 
     database = Database()
     acc_thread = threading.Thread(target=acceptation_thread, args=(s,database), daemon=True)
@@ -83,6 +112,8 @@ if __name__=="__main__":
             esc = input()
         except KeyboardInterrupt:
             exit()
+        if esc=="save":
+            database.close_all()
         if esc!="":
             exit()
 
